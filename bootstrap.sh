3a5853a696f594fb386510ca03d1ae202729bbb2
jtodd1 <james_todd@intuit.com>
2015-11-17 16:42:44 -0800

    mod: add more

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,21 +16,23 @@ web_formulas=('wget')
 web_casks=('google-chrome')
 package_formulas=('rpm')
 repository_formulas=('git' 'git-flow-avh' 'subversion' 'mercurial')
+repository_casks=('sourcetree')
 build_formulas=('maven' 'ant' 'sbt' 'gradle')
 language_formulas=('ruby' 'python' 'scala' 'go')
 # todo: caskroom/versions/java7
-language_casks=('java')
-container_formulas=('docker')
+language_casks=('java' 'caskroom/versions/java7')
+container_formulas=('docker' 'docker-compose' 'docker-machine' 'docker-swarm')
 container_casks=('virtualbox' 'dockertoolbox')
 hadoop_formulas=('hadoop' 'hbase' 'zookeeper')
 spark_formulas=('apache-spark')
 rdbms_formulas=('mysql')
 graph_formulas=('titan-server')
-ide_casks=('pycharm-ce' 'intellij-idea-ce' 'visualvm' 'rubymine')
+ide_casks=('pycharm-ce' 'intellij-idea-ce' 'visualvm' 'rubymine' 'atom')
 devops_formulas=('packer' 'consul' 'terraform' 'ansible' 'saltstack')
 # todo: hashi:atlas
 devops_casks=('vagrant' 'otto' 'nomad' 'serf' 'vault' 'vagrant-manager' 'chefdk')
-communications_casks=('skype')
+communications_casks=('skype' 'limechat' 'hipchat')
+miscellaneous_casks=('caffeine')
 
 for formula in ${make_formulas[@]} ${security_formulas[@]} ${web_formulas[@]} ${package_formulas[@]} ${repository_formulas[@]} ${build_formulas[@]} ${language_formulas[@]} ${container_formulas[@]} ${hadoop_formulas[@]} ${spark_formulas[@]} ${rdbms_formulas[@]} ${graph_formulas[@]} ${devops_formulas[@]}; do
   echo "$beers : brewing formula: $formula"
@@ -38,7 +40,7 @@ for formula in ${make_formulas[@]} ${security_formulas[@]} ${web_formulas[@]} ${
   echo "$beers : brewed formula: $formula"
 done
 
-for cask in ${web_casks[@]} ${language_casks[@]} ${container_casks[@]} ${ide_casks[@]} ${devops_casks[@]} ${communications_casks[@]}; do
+for cask in ${web_casks[@]} ${language_casks[@]} ${container_casks[@]} ${ide_casks[@]} ${devops_casks[@]} ${repository_casks[@]) ${communications_casks[@]}; do
   echo "$beers : brewing cask: $cask"
   brew cask install $cask; brew cask update $cask
   echo "$beers : brewing cask: $cask"
@@ -51,7 +53,8 @@ vagrant_plugins=('omnibus' 'ohai' 'berkshelf' 'hosts' 'cachier' 'aws')
 
 for vagrant_plugin in ${vagrant_plugins[@]}; do
   echo "$beers : vagranting plugin: $vagrant_plugin"
-  vagrant plugin install vagrant-$vagrant_plugin; vagrant plugin upgrade vagrant-$vagrant_plugin
+  vagrant plugin install vagrant-$vagrant_plugin
+  vagrant plugin update vagrant-$vagrant_plugin
   echo "$beers : vagranted plugin: $vagrant_plugin"
 done
 
@@ -61,8 +64,6 @@ done
 #go get code.google.com/p/go.tools/cmd/godoc
 
 gem install fpm
-
-sudo pip install scalr
 
 sudo pip install thefuck
 sudo pip install thefuck --upgrade
