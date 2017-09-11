bc1957287c5468ab4ffd289ff84fe2818480bc38
jwtodd <james.w.todd@gmail.com>
2017-09-11 14:59:52 -0700

    mod: +(ticktick,the-unarchiver)

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,6 +39,7 @@ misc_formulas=("jq" "watch")
 misc_casks=("coolterm" "tunnelblick" "tftpserver")
 x11_casks=("xquartz")
 formulas_with_default_names("gnu-sed")
+file_casks=("ticktick" "the-unarchiver")
 
 for formula in "${sh_formulas[@]}" "${make_formulas[@]}" "${security_formulas[@]}" "${web_formulas[@]}" "${aws_formulaas[@]}" "${package_formulas[@]}" "${repository_formulas[@]}" "${build_formulas[@]}" "${language_formulas[@]}" "${cassandra_formulas[@]}" "${hadoop_formulas[@]}" "${spark_formulas[@]}" "${rdbms_formulas[@]}" "${graph_formulas[@]}" "${devops_formulas[@]}" "${misc_formulas[@]}"; do
   echo "$beers : brewing formula: $formula"
